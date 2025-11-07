Added masking, finetuning, fixed normalisation in transformer block

src/model/transformer_block.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class TransformerBlock(nn.Module):
@@ -22,11 +23,12 @@ class TransformerBlock(nn.Module):
         )
 
     def forward(self, x):
-        y, _ = self.m_attention(self.layer_norm1(x), self.layer_norm1(x), self.layer_norm1(x))
+        attn_mask = torch.tril(torch.ones(x.size(1), x.size(1), device=x.device))
+        y, _ = self.m_attention(self.layer_norm1(x), self.layer_norm1(x), self.layer_norm1(x), attn_mask=attn_mask)
         y = self.dropout(y)
         
-        x = self.layer_norm2(x + y)
+        x = x + y
 
-        y = self.mlp(x)
+        y = self.mlp(self.layer_norm2(x))
         y = self.dropout(y)
         return x + y