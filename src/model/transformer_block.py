Fixed masking, additional small fixes

src/model/transformer_block.py
@@ -23,8 +23,13 @@ class TransformerBlock(nn.Module):
         )
 
     def forward(self, x):
-        attn_mask = torch.tril(torch.ones(x.size(1), x.size(1), device=x.device))
-        y, _ = self.m_attention(self.layer_norm1(x), self.layer_norm1(x), self.layer_norm1(x), attn_mask=attn_mask)
+        attn_mask = torch.triu(torch.ones(x.size(1), x.size(1), device=x.device), diagonal=1)
+        y, _ = self.m_attention(
+            self.layer_norm1(x), 
+            self.layer_norm1(x), 
+            self.layer_norm1(x), 
+            attn_mask=attn_mask
+        )
         y = self.dropout(y)
         
         x = x + y